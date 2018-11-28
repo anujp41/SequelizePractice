4fd4358c90c6f22f6cf3cb2920782d106e1b3aca
anujp41 <anuj.pant41@gmail.com>
2018-11-28 10:35:14 -0500

    barebones MASTER

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
-const db = require('./model').db;
 const PORT = 8000;
-const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api', routes);
-
 app.listen(PORT, () => {
-  console.log(`App listening on PORT:${PORT}`);
-  // db.sync({ force: true })
-  db.sync()
-    .then(() => console.log('Database is synched!'))
-    .catch(err => console.error('Trouble in lala land ', err.stack));
+  console.log(
+    `App listening on PORT:${PORT}
+    Currently I do not have any database;
+    Check other branches to see database & relations!
+    `
+  );
 });
